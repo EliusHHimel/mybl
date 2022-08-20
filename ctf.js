8961f973751dc679ab9679628e401678890929bb
Hm Elius Hossain Himel <48280169+EliusHHimel@users.noreply.github.com>
2022-08-20 22:40:04 +0600

    Update ctf.js

--- ctf.js
+++ ctf.js
@@ -4,11 +4,16 @@ let url2 = "https://assetliteapi.banglalink.net/api/v1/secret-token"
 
 fetch(url2).then(res => res.json()).then(res => {
     console.log(res)
-    let header = { secret_code: res.data.secret_code, _token: res.data._token }
+    let header = {
+        "Content-Type": "application/json",
+        "authorization": "Bearer " + res.data._token,
+        "client-security-token": `${res.data.secret_code + res.data._token}`,
+
+    }
 
     let data = { mobile: "01986461155" };
 
-    console.log(data)
+    console.log(header)
 
     fetch(url, {
         method: "POST",
